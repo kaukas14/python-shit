7decfc811f77e7ca795cf16ff44129b6b4da6694
kaukas14 <jjkaukas@gmail.com>
2018-08-06 21:35:26 +0300

    initial commit

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Karolis Grimaila'
+age = 26
+height = 188
+weight = 90
+eyes = 'grey'
+teeth = 'white'
+hair = 'browninsh'
+
+# su f galima naudoti kintamuosius i string
+print(f" Let's talk about { name}.")
+print(f" Its height is { height}.")
+print(f" Its weight is { weight}.")
+print(f" He's got { eyes} eyes and { hair} hair.")
+print(f" His teeth usally { teeth} unless drinks to much coffe.")
+
+total =  age +  height +  weight
+print(f" If I add { age}, { height} and { weight} I get {total}.")
