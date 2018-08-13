eecedfc12cc8d4f440a7a5af318c6e596f3be9a7
kaukas14 <jjkaukas@gmail.com>
2018-08-13 20:07:31 +0300

    input stuff

---
+++ ex11.py
@@ -0,0 +1,10 @@
+# Pradedam su input
+age = int(input('How old are you: ')) # galima taip gauti input i kintamaji
+print("How old are you?", end=' ') # arba galima ir taip idomu koks is esmes skirtumas ??
+age = int(input())
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.") ## prisiminimas kad f pradio leidzia iterpti variable i output string
---
+++ ex12.py
@@ -0,0 +1,6 @@
+#Realiai pratesimas 11 ex
+age = input('How old are you? ')
+height = input('How tall are you? ')
+weight = input('How much do you weigth ? ')
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy")
