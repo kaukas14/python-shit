428be9806d25fda843cbb67b474ffcd655a3f16a
kaukas14 <jjkaukas@gmail.com>
2018-08-15 16:36:46 +0300

    typo

--- ex14.py
+++ ex14.py
@@ -15,7 +15,7 @@ print("What kind of computer do you have?")
 computer = input(prompt)
 
 print(f"""
-Alright, so you said {likes} about liking me/
+Alright, so you said {likes} about liking me
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice
 """)
