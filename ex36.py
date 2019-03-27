6b26b5d17c3a68af33730e6b1abea3d69f048cc1
kaukas14 <jjkaukas@gmail.com>
2019-03-27 21:00:40 +0200

    pamokeles

---
+++ ex36.py
@@ -0,0 +1,18 @@
+# Patarimai su if ciklu
+# 1. Every if statement must have an else.
+# 2. If this else should never run because it doest make sense then you must use a die function
+# in the else that prints out an error message and dies, just like we did in the last exercise. This will find many errors.
+# 3. Never nest if statements more than two deep and always try to do them one deep.
+# 4. Treat if-statements like paragraphs where each if-elif-else grouping is like a set of sentences. Put blank lines before and after 
+# 5. Your boolean test should be simple. If they are complex, move their calculations to variables earlier in your function and use a good name for the variable.
+
+# Patarimai for ciklam
+# 1. Use while loop only yo loop forever and that mean probably never. This only applies to Python; other languages are different.
+# 2. Use a for-loop for all other kinds of looping especially if there is a fixed or limited number of things to loop over.
+
+# Patarimai debugint.
+# 1. Do not use a "debugger". A debugger is like doing a full body scan on a sick person. You do not get any specific useful information
+# and you find a whole lot of information that doesnt help and is just confusing.
+# 2. The best way to debug a program is to use print to print out the values of variables at points in the program to see where they go wrong.
+# 3. Make sure parts of your programs work as you work on them. Do not write massive files of code before you try to run them. Code a little, run a little, fix a little.
+
